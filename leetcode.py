fc7be26d7d90d0a7f842670dc15285d720547f03
OlyaKle <144671835+OlyaKle@users.noreply.github.com>
2024-05-03 17:16:22 +0300

    Add files via upload

--- leetcode.py
+++ leetcode.py
@@ -1,11 +1,29 @@
 class Solution:
     def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
-        cnt = Counter(letters)
-        n = len(words)
-        ans = 0
-        for i in range(1 << n):
-            cur = Counter(''.join([words[j] for j in range(n) if i >> j & 1]))
-            if all(v <= cnt[c] for c, v in cur.items()):
-                t = sum(v * score[ord(c) - ord('a')] for c, v in cur.items())
-                ans = max(ans, t)
-        return ans
+        def backtrack(index, letter_count, curr_sum):
+            if index == len(words):
+                self.ans = max(self.ans, curr_sum)
+                return
+
+            word = words[index]
+            temp = letter_count[:]
+            curr_word_sum = 0
+            valid = True
+            for c in word:
+                if letter_count[ord(c) - 97] == 0:
+                    valid = False
+                    break
+                curr_word_sum += score[ord(c) - 97]
+                letter_count[ord(c) - 97] -= 1
+            if valid:
+                backtrack(index + 1, letter_count, curr_sum + curr_word_sum)
+            letter_count = temp[:]
+            backtrack(index + 1, letter_count, curr_sum)
+
+        letter_count = [0] * 26
+        for c in letters:
+            letter_count[ord(c) - 97] += 1
+
+        self.ans = 0
+        backtrack(0, letter_count, 0)
+        return self.ans
